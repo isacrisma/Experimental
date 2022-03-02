e08e6230e769574e6a45d446d1fae5876db7810d
Isabel Martinez <99261842+isacrisma@users.noreply.github.com>
2022-03-02 13:55:58 +0100

    Update Kenya RCT - Code.R

--- Kenya RCT - Code.R
+++ Kenya RCT - Code.R
@@ -27,48 +27,69 @@ Data_Kenya <- Martinez_Glandel %>%
   mutate(Treatment = ifelse(AlreadyFodder == 1, 
                            3, Treatment))
 
-## Multiple t-tests for Age, Education, Land Size and Workers (continuous).
+## Drop observations where Treatment = 0 (Control)
 
-lapply(Data_Kenya[,c("Age", "Education", "LandSize", "Workers")], function(x) t.test(x ~ Data_Kenya$Treatment, var.equal = TRUE))
-
-## Chi-square test for Gender, Marital Status and Legal Status (categorical)
-
-chisq.test(Data_Kenya$Treatment, Data_Kenya$Gender, correct=FALSE)
-chisq.test(Data_Kenya$Treatment, Data_Kenya$Marriage, correct=FALSE)
-chisq.test(Data_Kenya$Treatment, Data_Kenya$Legal, correct=FALSE)
-
-## Summary Statistics - Tables
+Data_Kenya_0 <-subset(Data_Kenya,Treatment!="0")
 
 ## Drop observations where Treatment = 3 (already fodder)
 
 Data_Kenya_3 <-subset(Data_Kenya,Treatment!="3")
 
+## Multiple t-tests for Age, Education, Land Size and Workers (continuous).
+
+lapply(Data_Kenya_3[,c("Age", "Education", "LandSize", "Workers")], function(x) t.test(x ~ Data_Kenya_3$Treatment, var.equal = TRUE))
+
+lapply(Data_Kenya_0[,c("Age", "Education", "LandSize", "Workers")], function(x) t.test(x ~ Data_Kenya_0$Treatment, var.equal = TRUE))
+
+## Chi-square test for Gender, Marital Status and Legal Status (categorical) 
+## Dropping Treatment = 3 
+
+chisq.test(Data_Kenya_3$Treatment, Data_Kenya_3$Gender, correct=FALSE)
+chisq.test(Data_Kenya_3$Treatment, Data_Kenya_3$Marriage, correct=FALSE)
+chisq.test(Data_Kenya_3$Treatment, Data_Kenya_3$Legal, correct=FALSE)
+
+## Dropping Control 
+
+chisq.test(Data_Kenya_0$Treatment, Data_Kenya_0$Gender, correct=FALSE)
+chisq.test(Data_Kenya_0$Treatment, Data_Kenya_0$Marriage, correct=FALSE)
+chisq.test(Data_Kenya_0$Treatment, Data_Kenya_0$Legal, correct=FALSE)
+
+
+## Summary Statistics - Tables
+
 ## Get summary statistics for Treatment and Control
 
 library(smd)
 
-Data_Kenya_3 %>% dplyr::select(-BeliefFodder) %>% 
+Table1 <- Data_Kenya_3 %>% dplyr::select(-BeliefFodder) %>% 
   dplyr::select(-AlreadyFodder) %>%
   dplyr::select(-PostFodder) %>%
 tbl_summary(by = Treatment) %>% 
 add_difference() %>%
   modify_header(label ~ "**Characteristic**") %>%
   modify_spanning_header(c("stat_1") ~ "**Control Group**") %>%
+  modify_spanning_header(c("stat_2") ~ "**Treatment Group**") %>%
   modify_caption("**Table 1. Comparisons of Demographic and Land Characteristics between
-Treatment, Control and Omitted Group.**") %>%
-  bold_labels()
+Treatment and Control Group.**") %>%
+  bold_labels() 
 
-## Drop observations where Treatment = 0 (Control)
-
-Data_Kenya_0 <-subset(Data_Kenya,Treatment!="0")
+library(Rcpp)
+library(officer)
+library(flextable)
 
 ## Get summary statistics for Treatment and Already Agroforestry (Treatment = 3)
 
-Data_Kenya_0 %>% dplyr::select(-BeliefFodder) %>% 
+Table1.1 <- Data_Kenya_0 %>% dplyr::select(-BeliefFodder) %>% 
   dplyr::select(-AlreadyFodder) %>%
   dplyr::select(-PostFodder) %>%
   tbl_summary(by = Treatment) %>% 
-  add_p()
+  add_difference() %>%
+  modify_header(label ~ "**Characteristic**") %>%
+  modify_spanning_header(c("stat_1") ~ "**Treatment Group**") %>%
+  modify_spanning_header(c("stat_2") ~ "**Omitted Group**") %>%
+  modify_caption("**Table 1.1. Comparisons of Demographic and Land Characteristics between
+Treatment and Omitted Group.**") %>%
+  bold_labels() 
 
 ## Summary Statistics - Figures
 
@@ -79,66 +100,77 @@ library(ggplot2)
 ## 1. Land Size (Box Plot)
 
 boxplot(LandSize ~ Treatment, data = Data_Kenya,
-        horizontal = T,
-        col = c("#FFE0B2", "#FFA726"),
-        ylim = c(400, 90000))
+        horizontal = F,
+        col = c("#FF4500", "#FFA500","#FFD700"),
+        ylim = c(400, 85000),
+        names=c("Control","Treatment","Omitted"),
+        ylab ="Land Size (sqm)",
+        xlab = "Group") 
 
 ## 1. Land Size (Bar Plot)
 
-ggplot(Data_Kenya, aes(x = LandSize)) +
-  geom_histogram(fill = "white", colour = "black", bins = 15, binwidth = 3000) +
-  facet_grid(Treatment ~ .)
+Names <- Data_Kenya %>%
+  mutate(Treatment = recode(Treatment, "1" = "Treatment", "0" = "Control", "3" = "Omitted"))
+
+ggplot(Names, aes(x = LandSize)) +
+  geom_histogram(fill = "white", colour = "black", bins = 10, binwidth = 3000) +
+    facet_grid(Treatment~.) + labs(x = "Land Size (sqm)", y ="Frequency") +  theme(strip.background = element_rect(fill = "#FFA500"))  
 
 ## 2. Workers (Box Plot)
 
 boxplot(Workers ~ Treatment, data = Data_Kenya,
-        col = c("#FFE0B2", "#FFA726"),
-        horizontal = T,
-        ylim = c(0, 64))
+        col = c("#FF4500", "#FFA500","#FFD700"),
+        horizontal = F,
+        ylim = c(0, 64),
+        names=c("Control","Treatment","Omitted"),
+        ylab ="Number of Workers",
+        xlab = "Group")
 
 ## 2. Workers (Bar Plot)
 
-library(ggplot)
-
-ggplot(Data_Kenya, aes(x = Workers)) +
-  geom_histogram(fill = "white", colour = "black", bins = 4, binwidth = 4) +
-  facet_grid(Treatment ~ .)
+ggplot(Names, aes(x = Workers)) +
+  geom_histogram(fill = "white", colour = "black", bins = 10, binwidth = 5) +
+  facet_grid(Treatment ~ .) + labs(x = "Number of Workers", y ="Frequency") +  theme(strip.background = element_rect(fill = "#FFA500"))  
 
 ## Demographic Characteristics
 
 ## 1. Age (Box Plot)
 
 boxplot(Age ~ Treatment, data = Data_Kenya,
-        horizontal = T,
-        col = c("#FFE0B2", "#FFA726"),
-        ylim = c(0, 90))
+        horizontal = F,
+        col = c("#FF4500", "#FFA500","#FFD700"),
+        ylim = c(0, 90),
+        names=c("Control","Treatment","Omitted"),
+        ylab ="Age",
+        xlab = "Group")
 
 ## 1. Age (Bar Plot)
 
-ggplot(Data_Kenya, aes(x = Age)) +
-  geom_histogram(fill = "white", colour = "black", bins = 5, binwidth = 10) +
-  facet_grid(Treatment ~ .)
+ggplot(Names, aes(x = Age)) +
+  geom_histogram(fill = "white", colour = "black", bins = 10, binwidth = 5) +
+  facet_grid(Treatment ~ .) + labs(x = "Age", y ="Frequency") +  theme(strip.background = element_rect(fill = "#FFA500"))  
 
-## 2. Gender (Bar Plot)
+## 2. Gender (Bar Plot) --> Maybe not necessary 
 
-library(ggplot)
-
-ggplot(Data_Kenya, aes(x = Gender)) +
-  geom_histogram(fill = "white", colour = "black", bins = 2, binwidth = 1) +
-  facet_grid(Treatment ~ .)
+ggplot(Names, aes(x = Gender)) +
+  geom_histogram(fill = "white", colour = "black", bins = 3, binwidth = 0.2) +
+  facet_grid(Treatment ~ .) + labs(x = "Gender", y ="Frequency") +  theme(strip.background = element_rect(fill = "#FFA500"))  
 
 ## 3. Education (Box Plot)
 
 boxplot(Education ~ Treatment, data = Data_Kenya,
-        horizontal = TRUE,
-        col = c("#FFE0B2", "#FFA726"),
-        ylim = c(0, 15))
+        horizontal = F,
+        col = c("#FF4500", "#FFA500","#FFD700"),
+        ylim = c(0, 16),
+        names=c("Control","Treatment","Omitted"),
+        ylab ="Years of Education",
+        xlab = "Group")
 
 ## 3. Education (Bar Plot)
 
-ggplot(Data_Kenya, aes(x = Education)) +
-  geom_histogram(fill = "white", colour = "black", bins = 2, binwidth = 1) +
-  facet_grid(Treatment ~ .)
+ggplot(Names, aes(x = Education)) +
+  geom_histogram(fill = "white", colour = "black", bins = 8, binwidth = 1) +
+  facet_grid(Treatment ~ .) +  labs(x = "Years of Education", y ="Frequency") +  theme(strip.background = element_rect(fill = "#FFA500"))
 
 ## Multiple comparisons 
 
@@ -147,7 +179,8 @@ ggplot(Data_Kenya, aes(x = Education)) +
 gender.mean_land <- t(tapply(Data_Kenya$LandSize,
                         list(Data_Kenya$Treatment, Data_Kenya$Gender), mean))
 
-barplot(gender.mean_land, col=c("darkblue","red"), beside=TRUE, legend=rownames(gender.mean_land))
+barplot(gender.mean_land, col=c("#FF4500", "#FFA500","#FFD700"), beside=TRUE, 
+        xlab = "Group", ylab = "Frequency", legend=rownames(gender.mean_land))
 
 ## 2. Gender & Legal (Table) --> Maybe not necessary, doesn't say much
 
@@ -176,7 +209,6 @@ Education_Already <- Data_Kenya %>%
   group_by(Education, AlreadyFodder) %>%
   summarise(n = n()) %>%
   mutate(freq = n / sum(n))
-
 
 ggplot(Data_Kenya, aes(x = LandSize)) +
   geom_histogram(fill = "white", colour = "black", bins = 15, binwidth = 3000) +
